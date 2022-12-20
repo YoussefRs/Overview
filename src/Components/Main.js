c732dc95df9863e8a534b6df8fbe52a11557e3a9
YoussefRs <rs.youssef24@gmail.com>
2022-12-20 01:22:57 +0100

    Zoom
    Zoom in/out

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,18 +4,46 @@ import { useState } from 'react';
 
 function Main() {
     const [resize, setResize] = useState(false)
+    const [show, setShow] = useState(false)
+    const [hideArrow, setHideArrow] = useState(false)
+
     const handleClick = () => {
         setResize(true)
+        setShow(true)
+        setHideArrow(true)
+    }
+    const resizeGrayBox = () => {
+        setResize(false)
+        setShow(false)
+        setHideArrow(false)
     }
   return (
     <MainContent>
         <p><em>SIDE WIDE ANNOUCEMENT MESSAGE!</em> - SALE / DISCOUNT <b>OFFER - <u>NEW PRODUCT HIGHLIGHT</u></b></p>
-        <i><img src="img/cross.ico" alt="close"></img></i>
-        <GrayBox style={{width : resize ? "100%" : ""}}>
-            <img src='img/arrow.ico' alt="" onClick = {() => handleClick()}></img>
-            
-        </GrayBox>
+            <GrayBox style={{width : resize ? "100%" : ""}}>
+                <img src='img/arrow.ico' alt="" onClick = {() => handleClick()} style={{display : hideArrow ? "none" : ""}}></img>
+                <i style={{display : show ? "" : "none"}}><img src="img/cross.ico" alt="close" onClick={() => resizeGrayBox()} ></img></i>
+                <ImageSection>
+                <img
+                    src='/img/cart.jpg'
+                    alt = ""
+                    style={{
+                        width: '200px',
+                        height: '200px',
+                        transition: 'transform 0.2s',
+                        transform: 'scale(1)',
+                      }}
+                      onMouseEnter={(event) => {
+                        event.currentTarget.style.transform = 'scale(2)';
+                      }}
+                      onMouseLeave={(event) => {
+                        event.currentTarget.style.transform = 'scale(1)';
+                      }}
+                    />
+                </ImageSection>
+            </GrayBox>
 
+        
     </MainContent>
   )
 }
@@ -24,27 +52,48 @@ height : 80vh;
 position : relative;
 margin-top : 60px;
 display : flex;
+justify-content : space-between;
 flex-direction : column;
     p {
         font-size : 15px;
         text-align : center;
     }
+    i {
+        height : 90%;
+        width : auto;
+        z-index : 3;
+    }
     
 `;
+
 const GrayBox = styled.div`
 background-color : rgb(170, 163, 163);
 height : 100%;
 width : 60%;
-position : aboslute;
 top : 0;
 display : flex;
+justify-content : end;
 align-items : center;
-justify-content : center;
+position: relative;
 border-radius : 10px;
 transition: width 200ms;
+
     img {
-        padding-left : 80%;
+        cursor : pointer;
     }
     
 `;
+const ImageSection = styled.div`
+position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+    img {
+        width : 70%
+        
+    }
+`;
+
+    
+
 export default Main
